da5d76e555c197d442097eef830dff9a618c6765
gsdkira <larreakelly@gmail.com>
2022-06-15 10:47:56 -0400

    first commit

---
+++ app.py
@@ -0,0 +1,99 @@
+from dash import Dash, dcc, html
+import plotly.express as px
+import pandas as pd
+import certifi
+
+certifi.where()
+'/usr/local/lib/python3.7/site-packages/certifi/cacert.pem'
+
+# app = Dash(__name__)
+
+# colors = {
+#     'background': 'black',
+#     'text': '#7FDBFF'
+# }
+
+# # assume you have a "long-form" data frame
+# # see https://plotly.com/python/px-arguments/ for more options
+# df = pd.DataFrame({
+#     "Fruit": ["Apples", "Oranges", "Bananas", "Apples", "Oranges", "Bananas"],
+#     "Amount": [4, 1, 2, 2, 4, 5],
+#     "City": ["SF", "SF", "SF", "Montreal", "Montreal", "Montreal"]
+# })
+
+# fig = px.bar(df, x="Fruit", y="Amount", color="City", barmode="group")
+
+# fig.update_layout(
+#     plot_bgcolor=colors['background'],
+#     paper_bgcolor=colors['background'],
+#     font_color=colors['text']
+# )
+
+# app.layout = html.Div(style={'backgroundColor': colors['background']}, children=[
+#     html.H1(
+#         children='Hello Kelly',
+#         style={
+#             'textAlign': 'center',
+#             'color': colors['text']
+#         }
+#     ),
+
+#     html.Div(children='Dash: A web application framework for your data.', style={
+#         'textAlign': 'center',
+#         'color': colors['text']
+#     }),
+
+#     dcc.Graph(
+#         id='example-graph-2',
+#         figure=fig
+#     )
+# ])
+
+# if __name__ == '__main__':
+#     app.run_server(debug=True)
+
+#CREATES A TABLE BUT CSV FILE NOT WORKING
+# df = pd.read_csv('https://gist.githubusercontent.com/chriddyp/c78bf172206ce24f77d6363a2d754b59/raw/c353e8ef842413cae56ae3920b8fd78468aa4cb2/usa-agricultural-exports-2011.csv')
+
+
+# def generate_table(dataframe, max_rows=10):
+#     return html.Table([
+#         html.Thead(
+#             html.Tr([html.Th(col) for col in dataframe.columns])
+#         ),
+#         html.Tbody([
+#             html.Tr([
+#                 html.Td(dataframe.iloc[i][col]) for col in dataframe.columns
+#             ]) for i in range(min(len(dataframe), max_rows))
+#         ])
+#     ])
+
+
+# app = Dash(__name__)
+
+# app.layout = html.Div([
+#     html.H4(children='US Agriculture Exports (2011)'),
+#     generate_table(df)
+# ])
+
+# if __name__ == '__main__':
+#     app.run_server(debug=True)
+
+
+app = Dash(__name__)
+
+df = pd.read_csv('https://gist.githubusercontent.com/chriddyp/5d1ea79569ed194d432e56108a04d188/raw/a9f9e8076b837d541398e999dcbac2b2826a81f8/gdp-life-exp-2007.csv')
+
+fig = px.scatter(df, x="gdp per capita", y="life expectancy",
+                 size="population", color="continent", hover_name="country",
+                 log_x=True, size_max=60)
+
+app.layout = html.Div([
+    dcc.Graph(
+        id='life-exp-vs-gdp',
+        figure=fig
+    )
+])
+
+if __name__ == '__main__':
+    app.run_server(debug=True, ssl_context=context)
